148cf68b9d7f5e65a26ee3c81757b9c7cdc47058
kurumi.m <kurumi0306maeda@gmail.com>
2023-06-20 10:12:15 +0000

    fizzbuzz.rb

---
+++ kadai-fizzbuzz.rb
@@ -0,0 +1,20 @@
+#メソッドの定義
+def fizzbuzz(num)
+ if num % 3 == 0
+  return "Fizz"
+ elsif num % 5 == 0
+  return "Buzz"
+ elsif (num % 3 == 0) && (num % 5 == 0)
+  return "FizzBuzz"
+ else 
+  return num 
+ end 
+end 
+num_max = 100
+(1..num_max).each do |num|
+  puts fizzbuzz(num)
+end 
+
+
+#メソッドの呼び出し
+fizzbuzz()
